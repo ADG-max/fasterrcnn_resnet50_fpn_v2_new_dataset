e916755a32fe98002a95ed60cd0f9b6d55e047b1
ADG-max <150793755+ADG-max@users.noreply.github.com>
2025-12-17 06:20:55 +0700

    Update engine.py

--- torch_utils/engine.py
+++ torch_utils/engine.py
@@ -46,7 +46,7 @@ def train_one_epoch(
         step_counter += 1
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        with torch.cuda.amp.autocast(enabled=scaler is not None):
+        with torch.amp.autocast(device_type=device.type, enabled=scaler is not None):
             loss_dict = model(images, targets)
             losses = sum(loss for loss in loss_dict.values())
 
